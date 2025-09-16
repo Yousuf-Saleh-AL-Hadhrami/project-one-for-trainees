1c8771e15cde4f9ef22f6062e68987e533aeaab1
Yousuf AL Hadhrami <izkiboy@gmail.com>
2025-09-16 10:04:39 +0400

    updated the project

--- index.php
+++ index.php
@@ -7,15 +7,6 @@
 </head>
 <body>
 
-<!-- This form is added in branch feature-x -->
-<form action="login.php" method="post">
-    <label for="username">Username</label>
-    <input type="text" name="username" id="username">
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password">
-    <button type="submit">Login</button>
-</form>
-
 <hr>
 <!-- Table -->
 
---
+++ login.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+<body>
+    
+
+<!-- This form is added in branch feature-x -->
+<form action="login.php" method="post">
+    <label for="username">Username</label>
+    <input type="text" name="username" id="username">
+    <label for="password">Password</label>
+    <input type="password" name="password" id="password">
+    <button type="submit">Login</button>
+</form>
+
+
+</body>
+</html>
